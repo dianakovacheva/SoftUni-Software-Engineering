e3dc1cc28dfd30af37d482b543e255b5bb5dcea0
Diana Marinova <100419164+Diana-Marinova@users.noreply.github.com>
2023-04-24 14:45:50 +0200

    Adds cubeController and router

--- 06-JS-Back-End/04-Workshop-ExpressJS-and-Templating/src/routes.js
+++ 06-JS-Back-End/04-Workshop-ExpressJS-and-Templating/src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const homeController = require('./controllers/homeController.js');
+const cubeController = require('./controllers/cubeController.js');
 const router = express.Router();
 
 router.get('/', homeController.index);
+router.use('/cube', cubeController);
 
 module.exports = router;
