62f4468a53cd7a933bd826726b4c3d55684432d2
Diana Marinova <100419164+Diana-Marinova@users.noreply.github.com>
2023-04-24 13:32:40 +0200

    Renders home page

--- 06-JS-Back-End/04-Workshop-ExpressJS-and-Templating/src/index.js
+++ 06-JS-Back-End/04-Workshop-ExpressJS-and-Templating/src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const handlerbars = require('express-handlebars');
 const app = express();
 const port = 5050;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
 app.use('/static', express.static('public'));
 
-app.listen(port, () => console.log(`App is listening on por ${port}`));
+app.engine('hbs', handlerbars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
