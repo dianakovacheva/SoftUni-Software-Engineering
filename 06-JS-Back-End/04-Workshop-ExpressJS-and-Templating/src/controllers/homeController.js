cb4e02bfabc3c1ea2bf5fb3f7a70dedc552b1731
Diana Marinova <100419164+Diana-Marinova@users.noreply.github.com>
2023-04-24 14:53:50 +0200

    Adds about page and hbs file extension

--- 06-JS-Back-End/04-Workshop-ExpressJS-and-Templating/src/controllers/homeController.js
+++ 06-JS-Back-End/04-Workshop-ExpressJS-and-Templating/src/controllers/homeController.js
@@ -1,3 +1,7 @@
 exports.index = ((req, res) => {
-    res.render('index');
+    res.render('index.hbs');
+});
+
+exports.about = ((req, res) => {
+    res.render('about.hbs');
 });
--- 06-JS-Back-End/04-Workshop-ExpressJS-and-Templating/src/routes.js
+++ 06-JS-Back-End/04-Workshop-ExpressJS-and-Templating/src/routes.js
@@ -4,6 +4,7 @@ const cubeController = require('./controllers/cubeController.js');
 const router = express.Router();
 
 router.get('/', homeController.index);
+router.get('/about', homeController.about);
 router.use('/cube', cubeController);
 
 module.exports = router;
