061a4c720b8651aeafae87921d56632e2eccad9d
Diana Marinova <100419164+Diana-Marinova@users.noreply.github.com>
2023-04-28 21:16:25 +0200

    Finds cubes with id

--- 06-JS-Back-End/Workshop-Cubicle/src/services/cubeService.js
+++ 06-JS-Back-End/Workshop-Cubicle/src/services/cubeService.js
@@ -14,11 +14,11 @@ exports.getAll = (search = '', fromInput, toInput) => {
     return result;
 };
 
-exports.getOne = (cubeId) => cubes[cubeId];
-
 exports.create = (cube) => {
     cubes.push({ _id: cubes[cubes.length - 1]._id + 1, ...cube });
     let textData = JSON.stringify(cubes, '', 4);
 
     return fs.writeFile(path.resolve('src', 'db.json'), textData, { encoding: 'utf-8' });
 };
+
+exports.getOne = (cubeId) => cubes.find(cube => cube._id === Number(cubeId));
