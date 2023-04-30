565be9284780aef49b776a6d819a220b7d435297
Diana Marinova <100419164+Diana-Marinova@users.noreply.github.com>
2023-04-30 21:04:56 +0200

    Creates cube model

--- 06-JS-Back-End/02-Workshop-Cubicle/src/index.js
+++ 06-JS-Back-End/02-Workshop-Cubicle/src/index.js
@@ -1,21 +1,14 @@
 const express = require('express');
-const handlerbars = require('express-handlebars');
 const app = express();
 const routes = require('./routes.js');
 const { initializeDatabase } = require('./config/database.js');
 
 const port = 5050;
 
+require('./config/handlebars.js')(app);
+
 app.use('/static', express.static('public'));
 app.use(express.urlencoded({ extended: false }));
-
-app.engine('hbs', handlerbars.engine({
-    extname: 'hbs',
-}));
-
-app.set('view engine', 'hbs');
-app.set('views', './src/views');
-
 app.use(routes);
 
 initializeDatabase()
