46ec056582ba0909aabb750f03bd2c1b570ae247
Diana Marinova <100419164+Diana-Marinova@users.noreply.github.com>
2023-05-02 14:42:11 +0200

    Creates accessory model

---
+++ 06-JS-Back-End/02-Workshop-Cubicle/src/models/Accessory.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const accessorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: {
+            validator: /^http/g,
+        },
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 120,
+    }
+});
+
+const Accessory = mongoose.model('Accessory', accessorySchema);
+
+module.exports = Accessory;
