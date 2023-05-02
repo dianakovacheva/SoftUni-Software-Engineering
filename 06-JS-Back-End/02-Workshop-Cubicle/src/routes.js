e8b2b37b6150a19e173122c74434daa3b0d8203e
Diana Marinova <100419164+Diana-Marinova@users.noreply.github.com>
2023-05-02 12:40:51 +0200

    Adds create accessory page

--- 06-JS-Back-End/02-Workshop-Cubicle/src/routes.js
+++ 06-JS-Back-End/02-Workshop-Cubicle/src/routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController.js');
 const cubeController = require('./controllers/cubeController.js');
+const accessoryController = require('./controllers/accessoryController.js');
 
 router.use('/', homeController);
 router.use('/cube', cubeController);
+router.use('/accessory', accessoryController);
 
 module.exports = router;
