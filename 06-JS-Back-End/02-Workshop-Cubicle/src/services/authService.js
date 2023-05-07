30991fa0dba7fe0d5210073420033f0258942fdb
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-05-07 21:15:23 +0200

    Creates jwt on successful login

--- 06-JS-Back-End/02-Workshop-Cubicle/src/services/authService.js
+++ 06-JS-Back-End/02-Workshop-Cubicle/src/services/authService.js
@@ -1,6 +1,10 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User.js');
+
 const saltRounds = 10;
+const secret = 'afjasgklösdgösldhöldhäöäh';
 
 exports.register = async ({ username, password, repeatPassword }) => {
     // TODO: Return form validation message
@@ -16,4 +20,33 @@ exports.register = async ({ username, password, repeatPassword }) => {
     });
 
     return createdUser;
+};
+
+exports.login = async ({ username, password }) => {
+    let user = await User.findOne({ username });
+
+    if (!user) {
+        // TODO: Add message
+        return;
+    };
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        return;
+    };
+
+    let result = new Promise((resolve, reject) => {
+        jwt.sign({ _id: user._id, username: user.username }, secret, { expiresIn: '2d' }, (err, token) => {
+
+            if (err) {
+                return reject(err);
+            };
+
+            resolve(token);
+        });
+
+    });
+
+    return result;
 };
