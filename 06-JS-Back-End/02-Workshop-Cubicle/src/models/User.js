b1fd862d239e99cb053d38b4399ada393a089ae9
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-05-12 14:54:35 +0200

    Adds additional validations to model User

--- 06-JS-Back-End/02-Workshop-Cubicle/src/models/User.js
+++ 06-JS-Back-End/02-Workshop-Cubicle/src/models/User.js
@@ -3,12 +3,23 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     emailAddress: {
         type: String,
-        required: true,
+        required: [true, 'Email address is required'],
+        unique: [true, 'Email address is already taken'],
+        validate: /[A-Za-z0-9 ]/,
+        minlength: 5,
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
+        validate: /[A-Za-z0-9 ]/,
+        minlength: 8,
     },
+});
+
+userSchema.virtual('repeatPassword').set(function (value) {
+    if (this.password !== value) {
+        throw new Error('Repeat password don\'t match password');
+    };
 });
 
 const User = mongoose.model('User', userSchema);
