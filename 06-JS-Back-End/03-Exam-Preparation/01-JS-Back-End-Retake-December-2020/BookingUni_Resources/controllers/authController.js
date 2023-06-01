983b3f21b73a73157f6b905d3360baaecc476cf6
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-01 12:06:01 +0200

    Adds additional password validation, must be at least 5 characters long

--- 06-JS-Back-End/03-Exam-Preparation/01-JS-Back-End-Retake-December-2020/BookingUni_Resources/controllers/authController.js
+++ 06-JS-Back-End/03-Exam-Preparation/01-JS-Back-End-Retake-December-2020/BookingUni_Resources/controllers/authController.js
@@ -19,7 +19,9 @@ authController.post("/register", async (req, res) => {
     if (req.body.username == "" || req.body.password == "") {
       throw new Error("All fields required");
     }
-
+    if (req.body.password.length < 5) {
+      throw new Error("Password must be at least 5 characters long");
+    }
     if (req.body.password != req.body.repass) {
       throw new Error("Passwords don't match");
     }
