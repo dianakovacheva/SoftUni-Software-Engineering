aa65b239a2ae82b84180e8528d53469ad2918e82
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-01 16:49:47 +0200

    Creates dynamic profile view

--- 06-JS-Back-End/03-Exam-Preparation/01-JS-Back-End-Retake-December-2020/BookingUni_Resources/controllers/profileController.js
+++ 06-JS-Back-End/03-Exam-Preparation/01-JS-Back-End-Retake-December-2020/BookingUni_Resources/controllers/profileController.js
@@ -1,8 +1,11 @@
+const { hasUser } = require("../middlewares/guards");
+
 const profileController = require("express").Router();
 
-profileController.get("/", (req, res) => {
+profileController.get("/", hasUser(), (req, res) => {
   res.render("profile", {
     title: "Profile Page",
+    user: req.user,
   });
 });
 
