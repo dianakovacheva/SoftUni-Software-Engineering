c4002b34da76ec521349686f0ce9be68f275f89f
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-02 12:47:45 +0200

    Deletes unneeded comment

--- 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/models/User.js
+++ 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/models/User.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-// TODO add User properties and validation according to assigment
 const userSchema = new Schema({
   username: {
     type: String,
