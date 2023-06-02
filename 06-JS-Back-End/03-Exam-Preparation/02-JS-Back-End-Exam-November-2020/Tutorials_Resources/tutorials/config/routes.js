ba1f545a048d382d0cbad2c7e29ff132b52b5f1f
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-02 15:40:36 +0200

    Adjusts the course controller

--- 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/config/routes.js
+++ 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/config/routes.js
@@ -1,8 +1,9 @@
 const authController = require("../controllers/authController");
+const courserController = require("../controllers/courseController");
 const homeController = require("../controllers/homeController");
 
 module.exports = (app) => {
   app.use("/", homeController);
   app.use("/auth", authController);
+  app.use("/course", courserController);
 };
- 
