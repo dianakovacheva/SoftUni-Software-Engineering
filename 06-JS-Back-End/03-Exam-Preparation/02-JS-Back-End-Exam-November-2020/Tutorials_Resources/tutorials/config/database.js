f9b27cfe00841f91432315cd11f69cc300afcf70
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-02 12:46:40 +0200

    Connects the right database

--- 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/config/database.js
+++ 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/config/database.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-// TODO change database according to assigment
-const CONNECTION_STRING = "mongodb://localhost:27017/scaffoldDb";
+const CONNECTION_STRING = "mongodb://localhost:27017/tutorials";
 
 module.exports = async (app) => {
   try {
