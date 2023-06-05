7d7a97b6bf2cdeef3cbd4be4f8721c6ffdd508c5
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-05 15:09:31 +0200

    Shows different views if there are any courses and if there are no courses

--- 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/models/Course.js
+++ 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/models/Course.js
@@ -5,18 +5,18 @@ const URL_PATTERN = /https?:\/\/./i;
 const courseSchema = new Schema({
   title: {
     type: String,
-    required: true,
+
     minlength: [4, "Course title must be at least 4 characters long"],
   },
   description: {
     type: String,
-    required: true,
+
     minlength: [20, "Description must be at least 20 characters long"],
     maxlength: [50, "Description must be at most 50 characters long"],
   },
   imageUrl: {
     type: String,
-    required: true,
+
     validate: {
       validator: (value) => URL_PATTERN.test(value),
       message: "Invalid URL",
@@ -24,7 +24,7 @@ const courseSchema = new Schema({
   },
   duration: {
     type: String,
-    required: true,
+    required: [true, "Duration is required"],
   },
   createdAt: {
     type: String,
@@ -36,8 +36,7 @@ const courseSchema = new Schema({
     ref: "User",
     default: [],
   },
-  owner: { type: Types.ObjectId },
-  ref: "User",
+  owner: { type: Types.ObjectId, ref: "User" },
 });
 
 courseSchema.index(
