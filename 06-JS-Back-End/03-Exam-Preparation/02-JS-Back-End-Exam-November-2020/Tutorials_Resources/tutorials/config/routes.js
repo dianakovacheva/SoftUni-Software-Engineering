ac4037b187b216a68a3dc3edf0c0ceab9780907a
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-06 09:11:39 +0200

    Adds guard functions

--- 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/config/routes.js
+++ 06-JS-Back-End/03-Exam-Preparation/02-JS-Back-End-Exam-November-2020/Tutorials_Resources/tutorials/config/routes.js
@@ -1,9 +1,10 @@
 const authController = require("../controllers/authController");
 const courserController = require("../controllers/courseController");
 const homeController = require("../controllers/homeController");
+const { hasUser, isGuest } = require("../middlewares/guards");
 
 module.exports = (app) => {
   app.use("/", homeController);
-  app.use("/auth", authController);
-  app.use("/course", courserController);
+  app.use("/auth", isGuest(), authController);
+  app.use("/course", hasUser(), courserController);
 };
