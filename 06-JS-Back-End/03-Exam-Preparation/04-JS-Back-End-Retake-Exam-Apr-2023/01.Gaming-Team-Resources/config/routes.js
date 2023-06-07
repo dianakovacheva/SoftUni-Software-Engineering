4af077e6b90d48c52059eebaacdf450deab0375e
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-07 11:47:05 +0200

    Adds "/catalog" rout

--- 06-JS-Back-End/03-Exam-Preparation/04-JS-Back-End-Retake-Exam-Apr-2023/01.Gaming-Team-Resources/config/routes.js
+++ 06-JS-Back-End/03-Exam-Preparation/04-JS-Back-End-Retake-Exam-Apr-2023/01.Gaming-Team-Resources/config/routes.js
@@ -1,8 +1,9 @@
 const authController = require("../controllers/authController");
+const catalogController = require("../controllers/catalogController");
 const homeController = require("../controllers/homeController");
 
 module.exports = (app) => {
   app.use("/", homeController);
+  app.use("/catalog", catalogController);
   app.use("/auth", authController);
 };
- 
