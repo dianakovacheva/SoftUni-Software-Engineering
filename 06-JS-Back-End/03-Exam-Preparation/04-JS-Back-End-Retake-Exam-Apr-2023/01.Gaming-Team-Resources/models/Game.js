677b588aa6f67716bff3d6585488ba7c7b3fcb47
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-10 00:36:31 +0200

    Adds validations to the Game model

--- 06-JS-Back-End/03-Exam-Preparation/04-JS-Back-End-Retake-Exam-Apr-2023/01.Gaming-Team-Resources/models/Game.js
+++ 06-JS-Back-End/03-Exam-Preparation/04-JS-Back-End-Retake-Exam-Apr-2023/01.Gaming-Team-Resources/models/Game.js
@@ -1,25 +1,42 @@
-const { Schema, model, Types } = require("mongoose");
+const {
+  Schema,
+  model,
+  Types,
+  SchemaType,
+  default: mongoose,
+} = require("mongoose");
+
+const URL_PATTERN =
+  /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/i;
 
 const gameSchema = new Schema({
   name: {
     type: String,
     required: true,
+    minlength: [4, "Name must be at least 4 characters long"],
   },
   imageUrl: {
     type: String,
     required: true,
+    validate: {
+      validator: (value) => URL_PATTERN.test(value),
+      message: "Invalid URL",
+    },
   },
   price: {
     type: Number,
     required: true,
+    min: [0, "Price must be a positive number"],
   },
   description: {
     type: String,
     required: true,
+    minlength: [10, "Description must be at least 10 characters long"],
   },
   genre: {
     type: String,
     required: true,
+    minlength: [2, "Genre must be at least 2 characters long"],
   },
   platform: {
     type: String,
