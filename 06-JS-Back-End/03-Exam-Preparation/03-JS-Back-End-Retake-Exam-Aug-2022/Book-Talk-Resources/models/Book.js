22409c565c0cf059b9e58e8b1f0cb738275f8f4f
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-12 14:55:13 +0200

    Adds validation to the Book model
    This reverts commit db1da1b06979b75844d53096fac13064f289cc6c.

--- 06-JS-Back-End/03-Exam-Preparation/03-JS-Back-End-Retake-Exam-Aug-2022/Book-Talk-Resources/models/Book.js
+++ 06-JS-Back-End/03-Exam-Preparation/03-JS-Back-End-Retake-Exam-Aug-2022/Book-Talk-Resources/models/Book.js
@@ -1,36 +1,25 @@
 const { Schema, model, Types } = require("mongoose");
 
-const URL_PATTERN =
-  /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/i;
-
 const bookSchema = new Schema({
   title: {
     type: String,
     required: true,
-    minlength: [2, "The Title should be at least 2 characters"],
   },
   author: {
     type: String,
     required: true,
-    minlength: [5, "The Author should be at least 5 characters"],
   },
   imageUrl: {
     type: String,
     required: true,
-    validate: {
-      validator: (value) => URL_PATTERN.test(value),
-      message: "Invalid URL",
-    },
   },
   bookReview: {
     type: String,
     required: true,
-    minlength: [10, "The Review should be a minimum of 10 characters long."],
   },
   genre: {
     type: String,
     required: true,
-    minlength: [3, "The Genre should be at least 3 characters"],
   },
   stars: {
     type: Number,
@@ -48,5 +37,19 @@ const bookSchema = new Schema({
     ref: "User",
   },
 });
+
+bookSchema.index(
+  { title: 1 },
+  {
+    collation: {
+      locale: "en",
+      trength: 2,
+    },
+  }
+);
+
+const Book = model("Book", bookSchema);
+
+module.exports = Book;
 
 // Note:  When a user adds a book to their Wishlist, their id is added to that collection (WishingList)
