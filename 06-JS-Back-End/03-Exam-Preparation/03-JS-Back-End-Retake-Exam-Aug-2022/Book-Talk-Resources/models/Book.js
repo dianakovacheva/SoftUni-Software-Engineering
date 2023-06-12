b4db05c67fd91b6a7e15ade208c277dfe75e740d
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-12 15:00:39 +0200

    Adds validation to the Book model

--- 06-JS-Back-End/03-Exam-Preparation/03-JS-Back-End-Retake-Exam-Aug-2022/Book-Talk-Resources/models/Book.js
+++ 06-JS-Back-End/03-Exam-Preparation/03-JS-Back-End-Retake-Exam-Aug-2022/Book-Talk-Resources/models/Book.js
@@ -1,25 +1,36 @@
 const { Schema, model, Types } = require("mongoose");
 
+const URL_PATTERN =
+  /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/i;
+
 const bookSchema = new Schema({
   title: {
     type: String,
     required: true,
+    minlength: [2, "The Title should be at least 2 characters"],
   },
   author: {
     type: String,
     required: true,
+    minlength: [5, "The Author should be at least 5 characters"],
   },
   imageUrl: {
     type: String,
     required: true,
+    validate: {
+      validator: (value) => URL_PATTERN.test(value),
+      message: "Invalid URL",
+    },
   },
   bookReview: {
     type: String,
     required: true,
+    minlength: [10, "The Review should be a minimum of 10 characters long."],
   },
   genre: {
     type: String,
     required: true,
+    minlength: [3, "The Genre should be at least 3 characters"],
   },
   stars: {
     type: Number,
