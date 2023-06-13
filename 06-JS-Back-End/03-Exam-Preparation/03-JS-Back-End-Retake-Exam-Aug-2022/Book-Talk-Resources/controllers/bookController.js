32e32fd5718daed65ee878d21d5fd7da880da4a0
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-13 09:23:37 +0200

    Adds books to wish list

--- 06-JS-Back-End/03-Exam-Preparation/03-JS-Back-End-Retake-Exam-Aug-2022/Book-Talk-Resources/controllers/bookController.js
+++ 06-JS-Back-End/03-Exam-Preparation/03-JS-Back-End-Retake-Exam-Aug-2022/Book-Talk-Resources/controllers/bookController.js
@@ -1,8 +1,50 @@
 const { hasUser } = require("../middlewares/guards");
-const { createBookReview } = require("../services/bookService");
+const {
+  createBookReview,
+  getBookById,
+  bookWishedByUser,
+} = require("../services/bookService");
 const { parseError } = require("../util/parser");
 
 const bookController = require("express").Router();
+
+bookController.get("/details/:id/wishList", hasUser(), async (req, res) => {
+  const book = await getBookById(req.params.id);
+
+  if (!book.wishingList) {
+    book.wishingList = [];
+  }
+
+  if (book) {
+    if (
+      book.owner.toString() != req.user._id.toString() &&
+      book.wishingList
+        .map((x) => x.toString())
+        .includes(req.user._id.toString()) == false
+    ) {
+      await bookWishedByUser(req.params.id, req.user._id);
+    }
+    res.redirect(`/book/details/${req.params.id}`);
+  }
+});
+
+bookController.get("/details/:id", async (req, res) => {
+  const book = await getBookById(req.params.id);
+
+  book.isAuthor = book.owner.toString() == req.user?._id.toString();
+
+  if (book.wishingList && book.wishingList.length > 0) {
+    book.hasWished = book.wishingList
+      .map((x) => x.toString())
+      .includes(req.user?._id.toString());
+  }
+
+  res.render("details", {
+    title: "Details Page",
+    user: req.user,
+    book,
+  });
+});
 
 bookController.get("/createReview", hasUser(), (req, res) => {
   res.render("create", {
--- 06-JS-Back-End/03-Exam-Preparation/03-JS-Back-End-Retake-Exam-Aug-2022/Book-Talk-Resources/services/bookService.js
+++ 06-JS-Back-End/03-Exam-Preparation/03-JS-Back-End-Retake-Exam-Aug-2022/Book-Talk-Resources/services/bookService.js
@@ -4,11 +4,24 @@ async function getAllBooks() {
   return Book.find({}).lean();
 }
 
+async function getBookById(bookId) {
+  return Book.findById(bookId).lean();
+}
+
 async function createBookReview(book) {
   return Book.create(book);
 }
 
+async function bookWishedByUser(bookId, userId) {
+  const book = await Book.findById(bookId);
+  book.wishingList.push(userId);
+
+  return book.save();
+}
+
 module.exports = {
   getAllBooks,
+  getBookById,
   createBookReview,
+  bookWishedByUser,
 };
