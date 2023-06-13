6b48b79b7e29ed570de1aa8508304f79c52d05bb
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-13 17:12:17 +0200

    Adds Crypto model and validations to it

---
+++ 06-JS-Back-End/03-Exam-Preparation/05-JS-Back-End-Exam-Jun-2022/Crypto-Trade-Resources/models/Crypto.js
@@ -0,0 +1,53 @@
+const { Schema, model, Types } = require("mongoose");
+
+const URL_PATTERN =
+  /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/i;
+
+const cryptoSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, "The Name should be at least two characters long"],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => URL_PATTERN.test(value),
+      message: "Invalid URL",
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [1, "The Price should be a positive number"],
+  },
+  cryptoDescription: {
+    type: String,
+    required: true,
+    minlength: [
+      10,
+      "The Description should be a minimum of 10 characters long",
+    ],
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+    emun: ["crypto-wallet", "credit-card", "debit-card", "paypal"],
+  },
+  buyACrypto: {
+    type: [Types.ObjectId],
+    ref: "User",
+    default: [],
+  },
+  owner: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+});
+
+// Note:  When a user buys crypto, their id is added to that collection (Buy a crypto)
+
+const Crypto = model("Crypto", cryptoSchema);
+
+module.exports = Crypto;
