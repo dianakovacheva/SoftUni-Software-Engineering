e1b95f96c18d39cf34db9857fff7c690cb763960
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-15 11:03:55 +0200

    Adds logic to the navigation bar for guest and login users

--- 06-JS-Back-End/03-Exam-Preparation/06-JS-Back-End-Exam/Petstagram-Resources/controllers/homeController.js
+++ 06-JS-Back-End/03-Exam-Preparation/06-JS-Back-End-Exam/Petstagram-Resources/controllers/homeController.js
@@ -1,6 +1,5 @@
 const homeController = require("express").Router();
 
-// TODO replace with real controller by assigment
 homeController.get("/", (req, res) => {
   res.render("home", {
     title: "Home Page",
