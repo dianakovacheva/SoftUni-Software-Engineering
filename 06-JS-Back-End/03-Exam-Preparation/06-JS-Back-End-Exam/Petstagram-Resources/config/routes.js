fe2998005c2bcdabb3addc1a4dc8051cad5a471b
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-15 22:11:08 +0200

    Adds "404 page not found" to the app

--- 06-JS-Back-End/03-Exam-Preparation/06-JS-Back-End-Exam/Petstagram-Resources/config/routes.js
+++ 06-JS-Back-End/03-Exam-Preparation/06-JS-Back-End-Exam/Petstagram-Resources/config/routes.js
@@ -2,10 +2,17 @@ const authController = require("../controllers/authController");
 const catalogController = require("../controllers/catalogController");
 const homeController = require("../controllers/homeController");
 const photoPostController = require("../controllers/photoController");
+const userController = require("../controllers/userController");
 
 module.exports = (app) => {
   app.use("/", homeController);
   app.use("/auth", authController);
   app.use("/catalog", catalogController);
   app.use("/photo", photoPostController);
+  app.use("/user", userController);
+  app.use(function (req, res) {
+    res.status(404).render("404", {
+      user: req.user,
+    });
+  });
 };
