c610a2b4f56aa693be6ad923431054af0e9b6bc1
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-16 22:10:33 +0200

    Adds "404 page not found" template

--- 06-JS-Back-End/03-Exam-Preparation/07-JS-Back-End-Exam-February-2022/Job-Ads-Resources/config/routes.js
+++ 06-JS-Back-End/03-Exam-Preparation/07-JS-Back-End-Exam-February-2022/Job-Ads-Resources/config/routes.js
@@ -8,4 +8,10 @@ module.exports = (app) => {
   app.use("/auth", authController);
   app.use("/allAds", allAdsController);
   app.use("/ad", adController);
+  app.use(function (req, res) {
+    res.status(404).render("404", {
+      title: "404 Page not found",
+      user: req.user,
+    });
+  });
 };
