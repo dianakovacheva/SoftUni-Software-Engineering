b53e264e40eaddd91b2c98b0b0dc80845ad07ef9
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-16 10:57:49 +0200

    Initial commit

---
+++ 06-JS-Back-End/03-Exam-Preparation/07-JS-Back-End-Exam-February-2022/Job-Ads-Resources/controllers/authController.js
@@ -0,0 +1,75 @@
+const { register, login } = require("../services/userService");
+const { parseError } = require("../util/parser");
+
+const authController = require("express").Router();
+
+authController.get("/register", (req, res) => {
+  // TODO replace with actual view by assigment
+  res.render("register", {
+    title: "Register Page",
+  });
+});
+
+authController.post("/register", async (req, res) => {
+  try {
+    if (req.body.username == "" || req.body.password == "") {
+      throw new Error("All fields required");
+    }
+
+    if (req.body.password != req.body.repass) {
+      throw new Error("Passwords don't match");
+    }
+
+    const token = await register(req.body.username, req.body.password);
+
+    // TODO check assignment to see if register creates a session
+    res.cookie("token", token);
+
+    // TODO replace with redirect by assignment
+    res.redirect("/");
+  } catch (error) {
+    const errors = parseError(error);
+    // TODO add error desplay to actual template from assigment
+    res.render("register", {
+      title: "Register Page",
+      errors,
+      body: {
+        username: req.body.username,
+      },
+    });
+  }
+});
+
+authController.get("/login", (req, res) => {
+  // TODO replace with actual view by assigment
+  res.render("login", {
+    title: "Login Page",
+  });
+});
+
+authController.post("/login", async (req, res) => {
+  try {
+    const token = await login(req.body.username, req.body.password);
+    res.cookie("token", token);
+
+    // TODO replace with redirect by assignment
+    res.redirect("/");
+  } catch (error) {
+    const errors = parseError(error);
+    // TODO add error desplay to actual template from assigment
+    res.render("login", {
+      title: "Login Page",
+      errors,
+      body: {
+        username: req.body.username,
+      },
+    });
+  }
+});
+
+authController.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/");
+});
+
+module.exports = authController;
