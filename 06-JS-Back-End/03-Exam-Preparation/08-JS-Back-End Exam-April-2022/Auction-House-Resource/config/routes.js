b3d76401d0784589440cfbad5c11e677ff25a98a
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-06-28 15:05:50 +0200

    Creates dynamic templates

--- 06-JS-Back-End/03-Exam-Preparation/08-JS-Back-End Exam-April-2022/Auction-House-Resource/config/routes.js
+++ 06-JS-Back-End/03-Exam-Preparation/08-JS-Back-End Exam-April-2022/Auction-House-Resource/config/routes.js
@@ -1,9 +1,11 @@
 const auctionController = require("../controllers/auctionController");
 const authController = require("../controllers/authController");
+const browseController = require("../controllers/browseController");
 const homeController = require("../controllers/homeController");
 
 module.exports = (app) => {
   app.use("/", homeController);
   app.use("/auth", authController);
+  app.use("/browse", browseController);
   app.use("/auction", auctionController);
 };
