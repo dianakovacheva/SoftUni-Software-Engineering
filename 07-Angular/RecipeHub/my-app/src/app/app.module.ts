23725d2a47b9731f9de81e49e2bea8f5e1b80481
Diana Marinova <100419164+dianakovacheva@users.noreply.github.com>
2023-07-12 14:50:06 +0200

    Adds static header component with static navigation to the app

--- 07-Angular/RecipeHub/my-app/src/app/app.module.ts
+++ 07-Angular/RecipeHub/my-app/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../app/app.component';
 import { CoreComponent } from './core/core.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, CoreComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, CoreComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, HeaderComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 07-Angular/RecipeHub/my-app/src/app/core/header/header.component.ts
+++ 07-Angular/RecipeHub/my-app/src/app/core/header/header.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
+
+/**
+ * @title Basic toolbar
+ */
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTabsModule,
+  ],
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
